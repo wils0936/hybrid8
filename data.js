beadc5abe3911ff594e613a9bd3fb806b90bdc32
Jacob Wilson <thisisjacobwilson@gmail.com>
2020-10-19 11:50:37 -0400

    Add files 'main.js' and 'data.js'

---
+++ data.js
@@ -0,0 +1,22 @@
+/**
+ * Hybrid 7 JS file
+ * Name: Jacob Wilson
+ */
+
+const data = {
+  name: 'Jacob Wilson',
+  characters: [
+    { id: 1, name: 'Frodo', race: 'Hobbit' },
+    { id: 1, name: 'Gandalf', race: 'Wizard' },
+    { id: 1, name: 'Gimli', race: 'Dwarf' },
+    { id: 1, name: 'Legolas', race: 'Elf' },
+    { id: 1, name: 'Boromir', race: 'Human' },
+    { id: 1, name: 'TreeBeard', race: 'Ent' },
+    { id: 1, name: 'Gollum', race: 'Stoor Hobbit' },
+    { id: 1, name: 'Azog', race: 'Orc' },
+    { id: 1, name: 'Beorn', race: 'Shapeshifter' },
+    { id: 1, name: 'Shelob', race: 'Spider' },
+  ],
+};
+
+module.exports = data;
