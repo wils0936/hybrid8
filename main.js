6641ef8a10ef3f76311e804a56e0994af3833950
Jacob Wilson <thisisjacobwilson@gmail.com>
2020-10-19 16:22:51 -0400

    Finish the hybrid

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ const data = require('./data');
 
 const chars = data.characters.filter(item => item.race.indexOf("S"))
 
-console.log(data)
 console.log(chars)
+
+chars.sort((first, follow) => {
+    if( first.name > follow.name) return 1;
+    else if(follow.name > first.name) return -1;
+    else return 0;
+});
+console.log(chars);
