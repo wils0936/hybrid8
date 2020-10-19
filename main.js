598b8209691593b0c21a9cd01eea9cab82611654
Jacob Wilson <thisisjacobwilson@gmail.com>
2020-10-19 13:35:42 -0400

    Fix odd changes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// const { characters } = require('./data');
 // Jacob Wilson     wils0936@algonquinlive.com     Monday October 9th, 2020
 
 const data = require('./data');
 
-const chars = characters.filter(race => race)
+const chars = data.characters.filter(race => race)
 
 console.log(chars)
